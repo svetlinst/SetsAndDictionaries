0653e8fad3cacd34ab2e05c92cd5e8afc7524f83
Svetlin <svetlinst@gmail.com>
2016-11-27 18:56:05 +0200

    Commit

---
+++ P02-SetsOfElements/SetsElements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class SetsElements
+    {
+        static void Main()
+        {
+        string consoleInput = Console.ReadLine();
+        string[] numberOfEntries = consoleInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        HashSet<int> firstSet = new HashSet<int>();
+        HashSet<int> secondSet = new HashSet<int>();
+        for (int i = 0; i < int.Parse(numberOfEntries[0]); i++)
+        {
+            firstSet.Add(int.Parse(Console.ReadLine()));
+        }
+        for (int i = 0; i < int.Parse(numberOfEntries[1]); i++)
+        {
+            secondSet.Add(int.Parse(Console.ReadLine()));
+        }
+
+        HashSet<int> result = new HashSet<int>(firstSet.Intersect(secondSet));
+        foreach (var item in result)
+        {
+            Console.Write(item + " ");
+        }
+        }
+    }
+
