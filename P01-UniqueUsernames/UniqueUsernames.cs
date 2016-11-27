e05f1149044f68a5767cbcd637ed38ad67c5394f
Svetlin <svetlinst@gmail.com>
2016-11-27 18:43:22 +0200

    Commit

---
+++ P01-UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class UniqueUsernames
+    {
+        public static void Main(string[] args)
+        {
+        HashSet<string> usernames = new HashSet<string>();
+        int numberOfUsers = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numberOfUsers; i++)
+        {
+            usernames.Add(Console.ReadLine());
+        }
+        foreach (var name in usernames)
+        {
+            Console.WriteLine(name);
+        }
+
+        }
+    }
+
