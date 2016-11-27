37dcf1382f91ad20a1b98409f02b9d754eacb388
Svetlin <svetlinst@gmail.com>
2016-11-27 19:28:48 +0200

    Commit

---
+++ P04-CountSymbols/SymbolsCount.cs
@@ -0,0 +1,32 @@
+﻿//Write a program that reads some text from the console and counts the occurrences 
+//of each character in it. Print the results in alphabetical (lexicographical) order. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class SymbolsCount
+    {
+        public static void Main()
+        {
+        string textToRead = Console.ReadLine();
+        SortedDictionary<char, int> charOcc = new SortedDictionary<char, int>();
+        for (int i = 0; i < textToRead.Length; i++)
+        {
+            char currentChar = textToRead[i];
+            if (charOcc.ContainsKey(textToRead[i]) == false)
+            {
+                charOcc[currentChar] = 0;
+            }
+            charOcc[currentChar]++;
+        }
+        foreach (var kvp in charOcc)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+        }
+        }
+    }
+
