c48bcdcf0033ca46dc63cac180a588bd4103d26a
Svetlin <svetlinst@gmail.com>
2016-11-27 19:16:25 +0200

    Commit

---
+++ P03-PeriodicTable/PerTable.cs
@@ -0,0 +1,36 @@
+﻿//You are given an n number of chemical compounds. You need to keep track of all chemical 
+//elements used in the compounds and at the end print all unique ones in ascending order:
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class PerTable
+    {
+        public static void Main()
+        {
+        int numberOfElements = int.Parse(Console.ReadLine());
+        SortedSet<string> elements = new SortedSet<string>();
+        for (int i = 0; i < numberOfElements; i++)
+        {
+            string compound = Console.ReadLine();
+            string[] elementsInCompound = compound.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int j = 0; j < elementsInCompound.Length; j++)
+            {
+                elements.Add(elementsInCompound[j]);
+            }
+            
+        }
+
+
+        foreach (var item in elements)
+        {
+            Console.Write(item + " ");
+        }
+
+        }
+    }
+
